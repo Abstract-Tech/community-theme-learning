024fb252fb6452062f4b6e661398e8d0b8ae25d2
Laith massoud <laythmassoud8@gmail.com>
2022-07-22 16:04:29 +0200

    update the header

--- src/course-header/AnonymousUserMenu.jsx
+++ src/course-header/AnonymousUserMenu.jsx
@@ -4,14 +4,17 @@ import { getConfig } from '@edx/frontend-platform';
 import { getLoginRedirectUrl } from '@edx/frontend-platform/auth';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
 import { Button } from '@edx/paragon';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 import genericMessages from '../generic/messages';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
   
 function AnonymousUserMenu({ intl }) {
   return (
     <div>
-    <FontAwesomeIcon icon="far fa-eye" />
+       <FontAwesomeIcon icon={faUserCircle} className="d-md-none" size="lg" />
     <a className='courses-link' href={`${getConfig().LMS_BASE_URL}/courses`}>{intl.formatMessage(genericMessages.Explore)}</a>
       <Button
       className='login-button'
