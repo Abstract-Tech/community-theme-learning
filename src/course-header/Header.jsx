e745a777d70af195272edeb5d7b4a023fed43a48
Laith massoud <laythmassoud8@gmail.com>
2022-07-22 16:50:57 +0200

    update the header

--- src/course-header/Header.jsx
+++ src/course-header/Header.jsx
@@ -69,13 +69,13 @@ function Header({
           <span className="d-block small m-0">{courseOrg} {courseNumber}</span>
           <span className="d-block m-0 font-weight-bold course-title">{courseTitle}</span>
         </div>
-        {!authenticatedUser && (
+        {authenticatedUser && (
           <AuthenticatedUserDropdown
             enterpriseLearnerPortalLink={enterpriseLearnerPortalLink}
             username={authenticatedUser.username}
           />
         )}
-        {authenticatedUser && (
+        {!authenticatedUser && (
           <AnonymousUserMenu />
         )}
       </div>
