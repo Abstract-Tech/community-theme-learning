240b751c61da48aa3aedcb29c8f2de48712ddd18
Ghassan Maslamani <ghassan.maslamani@gmail.com>
2023-11-14 16:09:03 +0100

    feat: upgrade to quince

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -15,4 +15,16 @@ config.plugins.push(
   }),
 );
 
+config.devtool = false;
+
+config.cache = {
+  type: 'filesystem',
+  cacheDirectory: path.resolve(__dirname, '.cache'),
+};
+
+config.plugins = config.plugins
+  .filter(plugin => (plugin.constructor.name !== 'NewRelicPlugin'))
+  .filter(plugin => (plugin.constructor.name !== 'HtmlWebpackNewRelicPlugin'))
+  .filter(plugin => (plugin.constructor.name !== 'BundleAnalyzerPlugin'));
+
 module.exports = config;
