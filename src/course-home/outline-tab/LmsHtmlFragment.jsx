4a4a54116acdab50ab625a2d6773e7821e62096e
kal13 <lkallee@Abstract-technology.de>
2024-01-15 15:24:14 +0200

    updated de translations and added css code in index.scss

--- src/course-home/outline-tab/LmsHtmlFragment.jsx
+++ src/course-home/outline-tab/LmsHtmlFragment.jsx
@@ -3,6 +3,25 @@ import PropTypes from 'prop-types';
 
 import { getConfig } from '@edx/frontend-platform';
 
+window.onload = function() {
+  // Select the iframe
+  var iframe = document.querySelector('iframe');
+
+  // Access the content of the iframe
+  var iframeContent = iframe.contentDocument || iframe.contentWindow.document;
+
+  // Select all the <a> elements inside .small in the iframe's body
+  var links = iframeContent.querySelectorAll('body .small a');
+
+  // Check if the elements exist
+  if (links.length > 0) {
+      // Apply the desired styles
+      links.forEach(function(link) {
+          link.style.color = '#25a78e';
+      });
+  }
+};
+
 const LmsHtmlFragment = ({
   className,
   html,
@@ -45,7 +64,7 @@ const LmsHtmlFragment = ({
 
   return (
     <iframe
-      className="w-100 border-0"
+      className="w-100 border-0 handout-iframe"
       onLoad={resetIframeHeight}
       ref={iframe}
       referrerPolicy="origin"
